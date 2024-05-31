e35f403149a07da0da5bc81a2ca5b41456ae66c1
Belozertsev-av <andrew.vl.belozertsev@gmail.com>
2024-05-31 19:58:27 +0300

    new lesson has been ended

---
+++ WpfApp1/AddProduct.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для AddProduct.xaml
+    /// </summary>
+    public partial class AddProduct : Page
+    {
+        public AddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.appFrame.GoBack();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Cars editCar = new Cars()
+                {
+                    Id = AppFrame.cars.Capacity,
+                    carNumber = carNumberTextBox.Text.ToString(),
+                    type = typeTextBox.Text.ToString(),
+                    cost = Int32.Parse(costTextBox.Text),
+                    status = statusTextBox.Text.ToString()
+                };
+
+                if (editCar.Id != 0 && editCar.carNumber != "" && editCar.type != "" && editCar.cost != 0 && editCar.status != "")
+                {
+                    AppFrame.cars.Add(editCar);
+                    MessageBox.Show("Добавлен новый штраф c Id " + editCar.Id);
+                    AppFrame.appFrame.Navigate(new CarPage());
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Не заполнены все поля или заполнены не верно!");
+            }
+        }
+    }
+}
--- WpfApp1/AppFrame.cs
+++ WpfApp1/AppFrame.cs
@@ -10,5 +10,48 @@ namespace WpfApp1
     class AppFrame
     {
         public static Frame appFrame;
+
+        static Cars car1 = new Cars()
+        {
+            Id = 1,
+            carNumber = "BMW",
+            type = "превышение скорости",
+            cost = 5000,
+            status = "Не оплачен"
+        };
+        static Cars car2 = new Cars()
+        {
+            Id = 2,
+            carNumber = "Mersedes",
+            type = "Не правильная парковка",
+            cost = 2000,
+            status = "Не оплачен"
+        };
+        static Cars car3 = new Cars()
+        {
+            Id = 3,
+            carNumber = "Lada granta",
+            type = "Пересечение двойной сплошной",
+            cost = 100500,
+            status = "Оплачен"
+        };
+        static Cars car4 = new Cars()
+        {
+            Id = 4,
+            carNumber = "SAAB",
+            type = "Говняный цвет машины",
+            cost = 500,
+            status = "Оплачен"
+        };
+        static Cars car5 = new Cars()
+        {
+            Id = 5,
+            carNumber = "Волга",
+            type = "Колесо отлетело и разбило лобовуху Land Cruiser",
+            cost = 1_000_000,
+            status = "Не оплачен"
+        };
+
+        public static List<Cars> cars = new List<Cars>() { car1, car2, car3, car4, car5 };
     }
 }
